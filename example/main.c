465f9ee43dd9343749a573561bb0223a7e3afd15
Chris Eberly <ceberly@users.noreply.github.com>
2023-01-02 11:14:48 -0500

    save some work on the c example

--- example/main.c
+++ example/main.c
@@ -1,8 +1,24 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main (void) {
-  extern struct Arena *Arena;
+typedef struct Arena *Arena;
+
+extern void arena_init(Arena, void *, size_t size);
+extern void arena_alloc(Arena, void *, size_t size, size_t align);
+
+int main(void) {
+  void *store = malloc(1024);
+
+  Arena a = NULL;
+
+  arena_init(a, store, 1024);
+
+  int p = 32;
+  arena_alloc(a, &p, sizeof(int), sizeof(int));
+
+  p = 32;
+
+  fprintf(stderr, "%d\n", p);
 
   return EXIT_SUCCESS;
 }
