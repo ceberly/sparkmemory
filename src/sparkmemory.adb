adac6fdf341deab115fd96dcbf87ca96f2f01d75
Chris Eberly <ceberly@users.noreply.github.com>
2023-01-01 21:30:56 -0500

    make a c interface

--- src/sparkmemory.adb
+++ src/sparkmemory.adb
@@ -1,20 +1,6 @@
 package body Sparkmemory with
    SPARK_Mode => On
 is
-   function Align_Forward
-     (Addr : Address_Type; Align : Align_Type) return Address_Type with
-      Pre => Align mod 2 = 0
-   is
-      P : Address_Type := Addr;
-      M : Address_Type := P mod Align;
-   begin
-      if M /= 0 then
-         P := P + Align - M;
-      end if;
-
-      return P;
-   end Align_Forward;
-
    procedure Arena_Init (A : out Arena; Store : Address_Type; Size : Size_Type)
    is
    begin
@@ -25,17 +11,41 @@ is
 
    procedure Alloc_Align
      (A     : in out Arena; P : out Address_Type; Size : Size_Type;
-      Align :        Align_Type := Default_Alignment)
+      Align :        Align_Type)
    is
       UInt_Addr : Address_Type := A.Buf;
       Curr_Ptr  : Address_Type := UInt_Addr + A.Curr_Offset;
-      Offset    : Offset_Type  := Align_Forward (Curr_Ptr, Align) - UInt_Addr;
+      M         : Address_Type := Curr_Ptr mod Align;
+      Offset    : Offset_Type;
    begin
       P := To_Integer (System.Null_Address);
+
+      if M /= 0 then
+         Curr_Ptr := Curr_Ptr + Align - M;
+      end if;
+
+      Offset := Curr_Ptr - UInt_Addr;
 
       if Offset + Size <= A.Buf_Length then
          P             := UInt_Addr + Offset;
          A.Curr_Offset := Offset + Size;
       end if;
    end Alloc_Align;
+
+   procedure Alloc
+     (A : in out Arena; Addr : out System.Address; Size : Interfaces.C.size_t;
+      Align :        Interfaces.C.size_t)
+   is
+      P : Address_Type;
+   begin
+      Addr := System.Null_Address;
+
+      if Align < 1 or else Align mod 2 /= 0 then
+         return;
+      end if;
+
+      Alloc_Align (A, P, Size_Type (Size), Align_Type (Align));
+      Addr := To_Address (P);
+   end Alloc;
+
 end Sparkmemory;
--- src/sparkmemory.ads
+++ src/sparkmemory.ads
@@ -1,19 +1,19 @@
 with System;                  use System;
 with System.Storage_Elements; use System.Storage_Elements;
 
+with Interfaces.C; use Interfaces.C;
+
 package Sparkmemory with
    SPARK_Mode => On
 is
    type Arena is private;
 
-   subtype Address_Type is Integer_Address range 0 .. Integer_Address'Last;
+   subtype Address_Type is Integer_Address;
    -- type Address_Type is mod System.Memory_Size;
 
    subtype Size_Type is Address_Type;
    subtype Offset_Type is Address_Type;
    subtype Align_Type is Address_Type range 1 .. Address_Type'Last;
-
-   Default_Alignment : constant Align_Type := System.Address'Size * 2;
 
    procedure Arena_Init
      (A : out Arena; Store : Address_Type; Size : Size_Type) with
@@ -21,14 +21,20 @@ is
 
    procedure Alloc_Align
      (A     : in out Arena; P : out Address_Type; Size : Size_Type;
-      Align :        Align_Type := Default_Alignment) with
+      Align :        Align_Type) with
       Pre => Align mod 2 = 0;
+
+   procedure Alloc
+     (A : in out Arena; Addr : out System.Address; Size : Interfaces.C.size_t;
+      Align :        Interfaces.C.size_t) with
+      Export        => True,
+      Convention    => C,
+      External_Name => "arena_alloc_align";
 
 private
    type Arena is record
       Buf         : Address_Type := To_Integer (System.Null_Address);
       Buf_Length  : Size_Type    := 0;
       Curr_Offset : Offset_Type  := 0;
-
    end record;
 end Sparkmemory;
